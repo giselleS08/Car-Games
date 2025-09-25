956304d13744db0acfeb416ca4ef069a5177fc29
giselleS08 <giselle.sarabia8@gmail.com>
2025-09-25 14:22:58 +0000

    description of coinFlip function

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
+//target:
+// - set up functions for pages to turn uo
+// - describe each function
+// - start with coin flip
+// - then tic tac toe
+
+
+
 
 function setup() {
   createCanvas(800, 800);
   background(100);
 
+  headCoin = new Sprite(10,10,19,19)
+  tailCoin = new Sprite(10,10,19,19)
 }
 
 function draw() {
+  
+}
+
+function coinFlip()
+{
+// both get to decide whichever side they want 
+// ask player 1 if they want heads or tails
+// ask player 2 if they want heads or tails
+// animate the coin going up 
+// animated coin will just be a gray rectangle
+}
+
+function ticTacToe()
+{
+  
+}
+
+function rockPaperScissors()
+{
+  
+}
+
+function redLightGreenLight()
+{
   
 }
